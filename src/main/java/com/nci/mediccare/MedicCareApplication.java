5886d3f583b32d74522b7643301436635e27447a
DrFred23 <nyagiumar@gmail.com>
2023-10-12 17:04:51 +0100

    built the project medic-care

---
+++ src/main/java/com/nci/mediccare/MedicCareApplication.java
@@ -0,0 +1,13 @@
+package com.nci.mediccare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicCareApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nci/mediccare/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nci.mediccare;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MedicCareApplication.class);
+	}
+
+}
